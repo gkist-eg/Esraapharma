6b8ea302e0458fddc8cc22ce1ef78291c1f45a82
zahraa <alzahraagamal1@gmail.com>
2021-04-07 15:15:50 +0200

    item transfer name

--- item_transfer/__manifest__.py
+++ item_transfer/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Items Transfer gg",
+    'name': "GKIST Items Transfer",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
