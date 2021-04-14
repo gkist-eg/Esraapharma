465802fe04f6a8eb74d296962d7ef4f6d0133aca
zahraa <alzahraagamal1@gmail.com>
2021-04-14 10:29:59 +0200

    delivary report

--- inventory_update/models/models.py
+++ inventory_update/models/models.py
@@ -1,32 +1,26 @@
 import json
 import time
 from ast import literal_eval
-from collections import defaultdict
-from datetime import date
-from itertools import groupby
-from operator import itemgetter
 from odoo import SUPERUSER_ID, _, api, fields, models
 from odoo.addons.stock.models.stock_move import PROCUREMENT_PRIORITIES
-from odoo.exceptions import UserError
-from odoo.osv import expression
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT, format_datetime
-from odoo.tools.float_utils import float_compare, float_is_zero, float_round
-from odoo.tools.misc import format_date
 import json
 from collections import defaultdict
-from datetime import datetime
+from datetime import datetime,date
 from itertools import groupby
 from operator import itemgetter
 from re import findall as regex_findall
 from re import split as regex_split
 from dateutil import relativedelta
-from odoo import SUPERUSER_ID, _, api, fields, models
 from odoo.exceptions import UserError
 from odoo.osv import expression
 from odoo.tools.float_utils import float_compare, float_is_zero, float_repr, float_round
 from odoo.tools.misc import format_date, OrderedSet
-from re import findall as regex_findall
 
+
+class ProductTemplate(models.Model):
+    _inherit = 'stock.move'
+    pubprice = fields.Float("Customer Price", store=True, index=True)
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
