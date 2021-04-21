674838552278d8d280e9bd8a544ce4f9728915f4
zahraa <alzahraagamal1@gmail.com>
2021-04-21 11:23:35 +0200

    Quarantine Location Approval

--- quality_location/models/models.py
+++ quality_location/models/models.py
@@ -47,9 +47,9 @@ class StockPicking(models.Model):
     def _compute_show_validate(self):
         for picking in self:
             if picking.state == 'assigned' and picking.approve and self.location_id in self.env.user.stock_location_ids and self.location_dest_id in self.env.user.stock_location_ids:
-                if(self.location_id.stock_usage == 'qrtin' or self.location_dest_id.stock_usage == 'qrtin' ) and self.env.user.has_group('quality_location.group_stock_approve') and not picking.batch and self.picking_type_id.code != 'outgoing':
+                if(self.location_id.stock_usage == 'qrtin' or self.location_dest_id.stock_usage == 'qrtin' ) and self.env.user.has_group('quality_location.group_stock_approve') and not picking.batch and self.picking_type_id.code == 'internal':
                     picking.show_validate = True
-                elif(self.location_id.stock_usage == 'qrtin' or self.location_dest_id.stock_usage == 'qrtin' ) and self.env.user.has_group('stock.group_stock_manager') and not picking.batch and self.picking_type_id.code == 'outgoing':
+                elif(self.location_id.stock_usage == 'qrtin' or self.location_dest_id.stock_usage == 'qrtin' ) and self.env.user.has_group('stock.group_stock_manager') and not picking.batch and self.picking_type_id.code != 'internal':
                     picking.show_validate = True
                 elif (self.location_id.stock_usage == 'qrtin' or self.location_dest_id.stock_usage == 'qrtin' ) and self.env.user.has_group('stock.group_stock_manager') and picking.batch:
                     picking.show_validate = True
@@ -65,12 +65,12 @@ class StockPicking(models.Model):
     def compute_show_confirm(self):
         for picking in self:
             if picking.state == 'assigned' and not picking.approve and self.location_id in self.env.user.stock_location_ids and self.location_dest_id in self.env.user.stock_location_ids:
-                if(self.location_id.stock_usage == 'qrtin' or self.location_dest_id.stock_usage == 'qrtin' ) and self.env.user.has_group('quality_location.group_stock_approve') and not picking.batch and self.picking_type_id.code != 'outgoing':
+                if(self.location_id.stock_usage == 'qrtin' or self.location_dest_id.stock_usage == 'qrtin' ) and self.env.user.has_group('quality_location.group_stock_approve') and not picking.batch and self.picking_type_id.code  == 'internal':
                     picking.show_cofirm = True
                 elif (self.location_id.stock_usage == 'qrtin' or self.location_dest_id.stock_usage == 'qrtin') and self.env.user.has_group('stock.group_stock_user') and picking.batch:
                     picking.show_cofirm = True
 
-                elif (self.location_id.stock_usage == 'qrtin' or self.location_dest_id.stock_usage == 'qrtin' ) and self.env.user.has_group('stock.group_stock_user') and not picking.batch and self.picking_type_id.code == 'outgoing':
+                elif (self.location_id.stock_usage == 'qrtin' or self.location_dest_id.stock_usage == 'qrtin' ) and self.env.user.has_group('stock.group_stock_user') and not picking.batch and self.picking_type_id.code != 'internal':
                     picking.show_cofirm = True
 
                 elif self.location_dest_id.stock_usage != 'qrtin' and self.location_id.stock_usage != 'qrtin' and self.env.user.has_group('stock.group_stock_user'):
