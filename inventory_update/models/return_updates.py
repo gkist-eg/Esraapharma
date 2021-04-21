044865373e43cbbd8e1444a9f723b8d7321afd92
zahraa <alzahraagamal1@gmail.com>
2021-04-21 13:03:14 +0200

    return

--- inventory_update/models/return_updates.py
+++ inventory_update/models/return_updates.py
@@ -85,6 +85,22 @@ class ReturnPicking(models.TransientModel):
 
             }
 
+    def _prepare_move_default_values(self, return_line, new_picking):
+        vals = {
+            'product_id': return_line.product_id.id,
+            'product_uom_qty': return_line.quantity,
+            'product_uom': return_line.uom_id.id,
+            'picking_id': new_picking.id,
+            'state': 'draft',
+            'date': fields.Datetime.now(),
+            'location_id': return_line.move_id.location_dest_id.id,
+            'location_dest_id': self.location_id.id or return_line.move_id.location_id.id,
+            'picking_type_id': new_picking.picking_type_id.id,
+            'warehouse_id': self.picking_id.picking_type_id.warehouse_id.id,
+            'origin_returned_move_id': return_line.move_id.id,
+            'procure_method': 'make_to_stock',
+        }
+        return vals
 
     def _create_returns(self):
         # TODO sle: the unreserve of the next moves could be less brutal
