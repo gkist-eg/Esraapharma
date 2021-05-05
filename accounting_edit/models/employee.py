2676c25f90887660703e992d27aa698050ebae86
zahraa <alzahraagamal1@gmail.com>
2021-05-05 14:19:52 +0200

    employee

--- accounting_edit/models/employee.py
+++ accounting_edit/models/employee.py
@@ -7,3 +7,9 @@ class employyee_edit(models.Model):
     arabic_name = fields.Char(
         string='Arabic Name',
     )
+
+
+class user_edit(models.Model):
+    _inherit = 'res.users'
+    employee_arabic_name = fields.Char(related='employee_id.arabic_name', string="Arabic Name", readonly=False,
+                                       related_sudo=False)
