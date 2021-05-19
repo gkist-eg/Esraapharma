debb07287d3d71c169c12f3f541813f355a81c0e
zahraa <alzahraagamal1@gmail.com>
2021-05-19 11:00:27 +0200

    sales updates

--- serial_changed/models/models.py
+++ serial_changed/models/models.py
@@ -127,12 +127,16 @@ class Move(models.Model):
                 if self.move_type != 'entry':
                     if invoice.move_type == 'out_invoice' and invoice.warehouse_id.sale_store == False:
                         invoice.name = self.env['ir.sequence'].next_by_code('customer_invoice')
-                    elif invoice.move_type == 'out_invoice' and invoice.warehouse_id.sale_store == True:
-                        invoice.name = self.env['ir.sequence'].next_by_code('customer_invoice_distributor')
+                    # elif invoice.move_type == 'out_invoice' and invoice.warehouse_id.sale_store == True:
+                    #     invoice.name = self.env['ir.sequence'].next_by_code('customer_invoice_distributor')
                     elif invoice.move_type == 'out_refund':
                         invoice.name = self.env['ir.sequence'].next_by_code('refund_invoice')
                     elif invoice.move_type == 'in_refund':
                         invoice.name = self.env['ir.sequence'].next_by_code('refund_bill')
+                    else:
+                        invoice.name = self.env['ir.sequence'].next_by_code('customer_invoice_distributor')
+
+
 
         return super().action_post()
     # def action_post(self):
