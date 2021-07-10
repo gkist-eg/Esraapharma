71b87058a1d1b07686c0224cdb51bf93f1743198
zahraa <alzahraagamal1@gmail.com>
2021-07-10 14:05:50 +0200

    update purchase change price

--- purchase_request/models/edit_purchase_order.py
+++ purchase_request/models/edit_purchase_order.py
@@ -276,6 +276,53 @@ class EditPurchaseOrderLin(models.Model):
     categ_id = fields.Many2one('product.category', string='product Category', related="order_id.product_category_id")
     purchase_request_line = fields.Many2one("purchase.request.line", string="Purchase Requast Line", )
 
+    @api.onchange('product_qty', 'product_uom')
+    def _onchange_quantity(self):
+        if not self.product_id or self.state != 'draft':
+            return
+        params = {'order_id': self.order_id}
+        seller = self.product_id._select_seller(
+            partner_id=self.partner_id,
+            quantity=self.product_qty,
+            date=self.order_id.date_order and self.order_id.date_order.date(),
+            uom_id=self.product_uom,
+            params=params)
+
+        if seller or not self.date_planned:
+            self.date_planned = self._get_date_planned(seller).strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+
+        # If not seller, use the standard price. It needs a proper currency conversion.
+        if not seller:
+            po_line_uom = self.product_uom or self.product_id.uom_po_id
+            price_unit = self.env['account.tax']._fix_tax_included_price_company(
+                self.product_id.uom_id._compute_price(self.product_id.standard_price, po_line_uom),
+                self.product_id.supplier_taxes_id,
+                self.taxes_id,
+                self.company_id,
+            )
+            if price_unit and self.order_id.currency_id and self.order_id.company_id.currency_id != self.order_id.currency_id:
+                price_unit = self.order_id.company_id.currency_id._convert(
+                    price_unit,
+                    self.order_id.currency_id,
+                    self.order_id.company_id,
+                    self.date_order or fields.Date.today(),
+                )
+
+            self.price_unit = price_unit
+            return
+
+        price_unit = self.env['account.tax']._fix_tax_included_price_company(seller.price,
+                                                                             self.product_id.supplier_taxes_id,
+                                                                             self.taxes_id,
+                                                                             self.company_id) if seller else 0.0
+        if price_unit and seller and self.order_id.currency_id and seller.currency_id != self.order_id.currency_id:
+            price_unit = seller.currency_id._convert(
+                price_unit, self.order_id.currency_id, self.order_id.company_id, self.date_order or fields.Date.today())
+
+        if seller and self.product_uom and seller.product_uom != self.product_uom:
+            price_unit = seller.product_uom._compute_price(price_unit, self.product_uom)
+
+        self.price_unit = price_unit
 
 
     @api.depends('product_id')
