4395e91c0d63b7efe91c287f7ad792c191b4afc1
nourhan <noo146>
2021-07-24 15:15:43 +0200

    refund

--- custom_sale/models/refund.py
+++ custom_sale/models/refund.py
@@ -106,117 +106,62 @@ class Move(models.Model):
                 record.invoice_line_ids.dist_discount = record.partner_id.dist_discount
                 record.invoice_line_ids.cash_discount = record.partner_id.cash_discount
 
-    """@api.onchange('invoice_number', 'invoice_line_ids')
-    def _onchange_typ(self):
-        for record in self:
-            f = 0
-            if record.invoice_number:
-                data = []
-                res = []
-                for l in record.invoice_number.invoice_line_ids:
-                    res.append(l.product_id)
-                for line in record.invoice_line_ids:
-                    if line.product_id not in res:
-                        f += 1
-                        raise UserError(_(line.product_id.name + ' not in this invoice'))
-                    else:
-                        record.invoice_number = record.invoice_number
-                if f == 0:
-                    record.dist_discount = record.invoice_number.dist_discount
-                    record.cash_discount = record.invoice_number.cash_discount
-                    record.invoice_line_ids.discount = record.invoice_number.invoice_line_ids.discount
 
-                   # record.credit = record.partner_id.credit
-                    #record.office = record.invoice_number.office
-                    record.delivery_rep = record.invoice_number.delivery_rep
-                    record.sales_rep = record.invoice_number.sales_rep
-                    record.supervisor = record.invoice_number.supervisor
-                    record.pricelist_id = record.invoice_number.pricelist_id
-                    #record.fr = record.invoice_number.fr
-                    #record.category_type = record.invoice_number.category_type
-                    for line in record.invoice_line_ids:
-                        for l in record.invoice_number.invoice_line_ids:
-                            if l.product_id == line.product_id:
-                                line.price_unit = l.price_unit
-                                #line.bonus_price = l.bonus_price
-                                line.tax_ids = l.tax_ids
-                                line.discount = l.discount
-                                if line.quantity > l.quantity:
-                                  raise UserError(_('Quantity of ' + line.product_id.name + ' not exist in this invoice'))
 
-                        data.append((0,0,{
-                            'move_id': record.id,
-                            'product_id': line.product_id.id,
-                           # 'bonus_price': line.bonus_price,
-                            'tax_ids': [(6, 0, line.tax_ids.ids)],
-                            'price_unit': line.price_unit,
-                            'discount': line.discount,
-                            'name': line.name,
-                            'account_id': line.account_id,
-                            'quantity': line.quantity,
-                            #'uom_id': line.uom_id.id,
-                            #'batch_num': [(6, 0, line.batch_num.ids)],
-                            #'batch_no': [(6, 0, line.batch_no.ids)],
-                           # 'lot_id': line.lot_id.id,
-                            #'unit_tax': line.unit_tax,
-
-                        }))
-                    record.write({'invoice_line_ids': data})"""
-
-    @api.onchange('pricelist_id', 'invoice_line_ids')
-    def _onchange_price_list(self):
-
-        for record in self:
-            if record.move_type == 'out_refund' and record.pricelist_id:
-                if record.pricelist_id:
-                    for i in record.invoice_line_ids:
-                        partner = record.partner_id
-                        pricelist = record.pricelist_id
-                        product = i.product_id
-                        inv_date = record.invoice_date or date.today().strftime(DT)
-                        product = product.with_context(
-                            lang=partner.lang,
-                            partner=partner.id,
-                            quantity=i.quantity,
-                            date=inv_date,
-                            pricelist=pricelist.id,
-                            uom=i.product_uom_id.id,
-                        )
-                        product_context = dict(self.env.context, partner_id=record.partner_id.id,
-                                               date=record.invoice_date,
-                                               uom=i.product_uom_id.id)
-
-                        price, rule_id = pricelist.with_context(product_context).get_product_price_rule(
-                            i.product_id, i.quantity or 1.0, record.partner_id)
-                        new_list_price, currency_id = i.with_context(product_context)._get_real_price_currency(product,
-                                                                                                               rule_id,
-                                                                                                               i.quantity,
-                                                                                                               i.product_uom_id,
-                                                                                                               pricelist.id)
-                        discount = 0
-                        if new_list_price != 0:
-                            if pricelist.currency_id.id != currency_id:
-                                # we need new_list_price in the same currency as price, which is in the SO's pricelist's currency
-                                new_list_price = self.env['res.currency'].browse(currency_id).with_context(
-                                    product_context).compute(
-                                    new_list_price, pricelist.currency_id)
-                            discount += (new_list_price - price) / new_list_price * 100
-                            if discount > 0:
-                                i.discount = discount
-
-                        i.update({
-                            # 'move_id': record.id,
-                            'product_id': i.product_id.id,
-                            #'pricelist': pricelist.id,
-                            'price_unit': i.price_unit,
-                            'name': i.name,
-                            'discount': i.discount,
-                            'account_id': i.account_id,
-                            'product_uom_id': i.product_uom_id.id,
-                            #'sale_type': i.sale_type,
-                            #'tax_ids': [(6, 0, i.product_id.taxes_id.ids)],
-                            # 'batch_num': [(6, 0, i.batch_num.ids)],
-                            # 'batch_no': [(6, 0, i.batch_no.ids)],
-                            'lot_id': i.lot_id.id,
-
-                        })
+    # @api.onchange('pricelist_id', 'invoice_line_ids')
+    # def _onchange_price_list(self):
+    #
+    #     for record in self:
+    #         if record.move_type == 'out_refund' and record.pricelist_id:
+    #             if record.pricelist_id:
+    #                 for i in record.invoice_line_ids:
+    #                     partner = record.partner_id
+    #                     pricelist = record.pricelist_id
+    #                     product = i.product_id
+    #                     inv_date = record.invoice_date or date.today().strftime(DT)
+    #                     product = product.with_context(
+    #                         lang=partner.lang,
+    #                         partner=partner.id,
+    #                         quantity=i.quantity,
+    #                         date=inv_date,
+    #                         pricelist=pricelist.id,
+    #                         uom=i.product_uom_id.id,
+    #                     )
+    #                     product_context = dict(self.env.context, partner_id=record.partner_id.id,
+    #                                            date=record.invoice_date,
+    #                                            uom=i.product_uom_id.id)
+    #
+    #                     price, rule_id = pricelist.with_context(product_context).get_product_price_rule(
+    #                         i.product_id, i.quantity or 1.0, record.partner_id)
+    #                     new_list_price, currency_id = i.with_context(product_context)._get_real_price_currency(product,
+    #                                                                                                            rule_id,
+    #                                                                                                            i.quantity,
+    #                                                                                                            i.product_uom_id,
+    #                                                                                                            pricelist.id)
+    #                     discount = 0
+    #                     if new_list_price != 0:
+    #                         if pricelist.currency_id.id != currency_id:
+    #                             # we need new_list_price in the same currency as price, which is in the SO's pricelist's currency
+    #                             new_list_price = self.env['res.currency'].browse(currency_id).with_context(
+    #                                 product_context).compute(
+    #                                 new_list_price, pricelist.currency_id)
+    #                         discount += (new_list_price - price) / new_list_price * 100
+    #                         if discount > 0:
+    #                             i.discount = discount
+    #
+    #                     i.update({
+    #                         # 'move_id': record.id,
+    #                         'product_id': i.product_id.id,
+    #                         #'pricelist': pricelist.id,
+    #                         'price_unit': i.price_unit,
+    #                         'name': i.name,
+    #                         'discount': i.discount,
+    #                         'account_id': i.account_id,
+    #                         'product_uom_id': i.product_uom_id.id,
+    #                         #'sale_type': i.sale_type,
+    #                         #'tax_ids': [(6, 0, i.product_id.taxes_id.ids)],
+    #                         # 'batch_num': [(6, 0, i.batch_num.ids)],
+    #                         # 'batch_no': [(6, 0, i.batch_no.ids)],
+    #                         'lot_id': i.lot_id.id,
+    #
+    #                     })
