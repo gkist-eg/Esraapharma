257e3f47b5deefa9fca46724c945d1c00704ec62
nourhan <noo146>
2021-07-24 23:14:05 +0200

    return refund

--- custom_sale/models/refund.py
+++ custom_sale/models/refund.py
@@ -108,62 +108,62 @@ class Move(models.Model):
 
 
 
-    # @api.onchange('pricelist_id', 'invoice_line_ids')
-    # def _onchange_price_list(self):
-    #
-    #     for record in self:
-    #         if record.move_type == 'out_refund' and record.pricelist_id:
-    #             if record.pricelist_id:
-    #                 for i in record.invoice_line_ids:
-    #                     partner = record.partner_id
-    #                     pricelist = record.pricelist_id
-    #                     product = i.product_id
-    #                     inv_date = record.invoice_date or date.today().strftime(DT)
-    #                     product = product.with_context(
-    #                         lang=partner.lang,
-    #                         partner=partner.id,
-    #                         quantity=i.quantity,
-    #                         date=inv_date,
-    #                         pricelist=pricelist.id,
-    #                         uom=i.product_uom_id.id,
-    #                     )
-    #                     product_context = dict(self.env.context, partner_id=record.partner_id.id,
-    #                                            date=record.invoice_date,
-    #                                            uom=i.product_uom_id.id)
-    #
-    #                     price, rule_id = pricelist.with_context(product_context).get_product_price_rule(
-    #                         i.product_id, i.quantity or 1.0, record.partner_id)
-    #                     new_list_price, currency_id = i.with_context(product_context)._get_real_price_currency(product,
-    #                                                                                                            rule_id,
-    #                                                                                                            i.quantity,
-    #                                                                                                            i.product_uom_id,
-    #                                                                                                            pricelist.id)
-    #                     discount = 0
-    #                     if new_list_price != 0:
-    #                         if pricelist.currency_id.id != currency_id:
-    #                             # we need new_list_price in the same currency as price, which is in the SO's pricelist's currency
-    #                             new_list_price = self.env['res.currency'].browse(currency_id).with_context(
-    #                                 product_context).compute(
-    #                                 new_list_price, pricelist.currency_id)
-    #                         discount += (new_list_price - price) / new_list_price * 100
-    #                         if discount > 0:
-    #                             i.discount = discount
-    #
-    #                     i.update({
-    #                         # 'move_id': record.id,
-    #                         'product_id': i.product_id.id,
-    #                         #'pricelist': pricelist.id,
-    #                         'price_unit': i.price_unit,
-    #                         'name': i.name,
-    #                         'discount': i.discount,
-    #                         'account_id': i.account_id,
-    #                         'product_uom_id': i.product_uom_id.id,
-    #                         #'sale_type': i.sale_type,
-    #                         #'tax_ids': [(6, 0, i.product_id.taxes_id.ids)],
-    #                         # 'batch_num': [(6, 0, i.batch_num.ids)],
-    #                         # 'batch_no': [(6, 0, i.batch_no.ids)],
-    #                         'lot_id': i.lot_id.id,
-    #
-    #                     })
+    @api.onchange('pricelist_id', 'invoice_line_ids')
+    def _onchange_price_list(self):
+
+        for record in self:
+            if record.move_type == 'out_refund' and record.pricelist_id:
+                if record.pricelist_id:
+                    for i in record.invoice_line_ids:
+                        partner = record.partner_id
+                        pricelist = record.pricelist_id
+                        product = i.product_id
+                        inv_date = record.invoice_date or date.today().strftime(DT)
+                        product = product.with_context(
+                            lang=partner.lang,
+                            partner=partner.id,
+                            quantity=i.quantity,
+                            date=inv_date,
+                            pricelist=pricelist.id,
+                            uom=i.product_uom_id.id,
+                        )
+                        product_context = dict(self.env.context, partner_id=record.partner_id.id,
+                                               date=record.invoice_date,
+                                               uom=i.product_uom_id.id)
+
+                        price, rule_id = pricelist.with_context(product_context).get_product_price_rule(
+                            i.product_id, i.quantity or 1.0, record.partner_id)
+                        new_list_price, currency_id = i.with_context(product_context)._get_real_price_currency(product,
+                                                                                                               rule_id,
+                                                                                                               i.quantity,
+                                                                                                               i.product_uom_id,
+                                                                                                               pricelist.id)
+                        discount = 0
+                        if new_list_price != 0:
+                            if pricelist.currency_id.id != currency_id:
+                                # we need new_list_price in the same currency as price, which is in the SO's pricelist's currency
+                                new_list_price = self.env['res.currency'].browse(currency_id).with_context(
+                                    product_context).compute(
+                                    new_list_price, pricelist.currency_id)
+                            discount += (new_list_price - price) / new_list_price * 100
+                            if discount > 0:
+                                i.discount = discount
+
+                        i.update({
+                            # 'move_id': record.id,
+                            'product_id': i.product_id.id,
+                            #'pricelist': pricelist.id,
+                            'price_unit': i.price_unit,
+                            'name': i.name,
+                            'discount': i.discount,
+                            'account_id': i.account_id,
+                            'product_uom_id': i.product_uom_id.id,
+                            #'sale_type': i.sale_type,
+                            #'tax_ids': [(6, 0, i.product_id.taxes_id.ids)],
+                            # 'batch_num': [(6, 0, i.batch_num.ids)],
+                            # 'batch_no': [(6, 0, i.batch_no.ids)],
+                            'lot_id': i.lot_id.id,
+
+                        })
 
 
