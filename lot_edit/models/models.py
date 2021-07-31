b284a8c40cb2dcdb0408fa417d668854aef218e9
nourhan <noo146>
2021-07-31 10:14:53 +0200

    lot_edit_edit

--- lot_edit/models/models.py
+++ lot_edit/models/models.py
@@ -8,3 +8,11 @@ class lot_edit(models.TransientModel):
 
     qc_attach = fields.Binary()
 
+    def lot_edit(self):
+        context = dict(self._context or {})
+        active_ids = context.get('active_ids', []) or []
+
+        for record in self.env['stock.production.lot'].browse(active_ids):
+
+            record.lot_edit()
+        return {'type': 'ir.actions.act_window_close'}
