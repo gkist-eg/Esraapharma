623bdaa07e6a421e838fdd893998013d0c0c5e73
nourhan <noo146>
2021-07-31 14:34:31 +0200

    secuirty

--- lot_edit/models/models.py
+++ lot_edit/models/models.py
@@ -4,4 +4,14 @@ from odoo import models, fields, api
 class lot_edit_inhireit(models.Model):
     _inherit = 'stock.production.lot'
 
-    attachment_qc = fields.Many2many('ir.attachment','attachment_qc_lot_rel', string='QC Attachments', )
+    attachment_qc = fields.Many2many('ir.attachment', 'attachment_qc_lot_rel', string='QC Attachments', )
+
+
+class lot_edit(models.TransientModel):
+    _name = "stock.production.lot.confirm"
+    _description = "Confirm the selected plan"
+
+    def plan_confirm(self):
+        context = dict(self._context or {})
+        active_ids = context.get('active_ids', []) or []
+
