f6f8d2ff56f1f88e5f7396c68c4c8e56d4412096
nourhan <noo146>
2021-07-31 11:21:19 +0200

    delete

--- lot_edit/models/models.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api
-
-
-class lot_edit_inhireit(models.Model):
-    _inherit = 'stock.production.lot'
-
-    attachment_qc = fields.Many2many('ir.attachment', string='QC Attachments', )
