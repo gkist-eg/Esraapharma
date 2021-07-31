d9e4a3d6b22fe8f8dc5c73732a45efd72db0af5f
nourhan <noo146>
2021-07-31 11:12:37 +0200

    lot_update

--- lot_edit/models/models.py
+++ lot_edit/models/models.py
@@ -2,20 +2,6 @@
 from odoo import models, fields, api
 
 
-class lot_edit(models.TransientModel):
-    _name = 'stock.production.lot.edit'
-
-
-    qc_attach = fields.Binary()
-
-    def lot_edit(self):
-        context = dict(self._context or {})
-        active_ids = context.get('active_ids', []) or []
-
-        for record in self.env['stock.production.lot'].browse(active_ids):
-
-            record.button_approve_double_visit()
-        return {'type': 'ir.actions.act_window_close'}
 
 class lot_edit_inhireit(models.Model):
     _inherit = 'stock.production.lot'
