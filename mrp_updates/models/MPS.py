7eca57dd3f9fdac2e5c4da2b32790138c221ff4e
zahraa <alzahraagamal1@gmail.com>
2021-08-01 10:23:55 +0200

    mps views

--- mrp_updates/models/MPS.py
+++ mrp_updates/models/MPS.py
@@ -98,6 +98,42 @@ class MrpProductionSchedule(models.Model):
 
         return [tree for tree in indirect_demand_trees.values()]
 
+    @api.model
+    def get_mps_view_state(self, domain=False):
+        """ Return the global information about MPS and a list of production
+        schedules values with the domain.
+
+        :param domain: domain for mrp.production.schedule
+        :return: values used by the client action in order to render the MPS.
+            - dates: list of period name
+            - production_schedule_ids: list of production schedules values
+            - manufacturing_period: list of periods (days, months or years)
+            - company_id: user current company
+            - groups: company settings that hide/display different rows
+        :rtype: dict
+        """
+        productions_schedules = self.env['mrp.production.schedule'].search(domain or [])
+        productions_schedules.filtered(
+                lambda schedule: schedule.product_id.sale_ok == True)
+        productions_schedules_states = productions_schedules.get_production_schedule_view_state()
+        company_groups = self.env.company.read([
+            'mrp_mps_show_starting_inventory',
+            'mrp_mps_show_demand_forecast',
+            'mrp_mps_show_indirect_demand',
+            'mrp_mps_show_actual_demand',
+            'mrp_mps_show_to_replenish',
+            'mrp_mps_show_actual_replenishment',
+            'mrp_mps_show_safety_stock',
+            'mrp_mps_show_available_to_promise',
+        ])
+        return {
+            'dates': self.env.company._date_range_to_str(),
+            'production_schedule_ids': productions_schedules_states,
+            'manufacturing_period': self.env.company.manufacturing_period,
+            'company_id': self.env.company.id,
+            'groups': company_groups,
+        }
+
     def _get_replenish_qty(self, after_forecast_qty):
         """ Modify the quantity to replenish depending the min/max and targeted
         quantity for safety stock.
