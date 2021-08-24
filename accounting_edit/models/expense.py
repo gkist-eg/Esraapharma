b30a0e4341756ee7a5ee6194a2d0e162723b7de6
nourhan <noo146>
2021-08-24 13:16:46 +0200

    expen

--- accounting_edit/models/expense.py
+++ accounting_edit/models/expense.py
@@ -13,12 +13,6 @@ class ExpenseEdit(models.Model):
     product_id = fields.Many2one('product.product', string='Product', readonly=True, tracking=True, states={'draft': [('readonly', False)], 'reported': [('readonly', False)], 'approved': [('readonly', False)], 'refused': [('readonly', False)]}, domain="[('can_be_expensed', '=', True), '|', ('company_id', '=', False), ('company_id', '=', company_id)]", ondelete='restrict')
     user_id = fields.Many2one('res.users', 'Manager', compute='_compute_from_employee_id', store=True, readonly=True, copy=False, states={'draft': [('readonly', True)]}, tracking=True, domain=lambda self: [('groups_id', 'in', self.env.ref('hr_expense.group_hr_expense_team_approver').id)])
 
-    @api.depends('employee_id')
-    def _compute_from_employee_id(self):
-        for sheet in self:
-            sheet.address_id = sheet.employee_id.sudo().address_home_id
-            sheet.department_id = sheet.employee_id.department_id
-            sheet.user_id = sheet.employee_id.expense_manager_id or sheet.employee_id.parent_id.user_id
 
 
     def _get_employee_id_domain(self):
