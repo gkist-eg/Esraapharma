03d072f7ecdc9999db32fe1eb6f34a993633bd0f
zahraa <alzahraagamal1@gmail.com>
2021-08-28 12:58:17 +0200

    update mps

--- mrp_updates/models/MPS.py
+++ mrp_updates/models/MPS.py
@@ -95,69 +95,69 @@ class MrpProductionSchedule(models.Model):
             ('date', '<=', date_stop)
         ]
 
-    def _get_indirect_demand_tree(self):
-        """ Get the tree architecture for all the BoM and BoM line that are
-        related to production schedules in self. The purpose of the tree:
-        - Easier traversal than with BoM and BoM lines.
-        - Allow to determine the schedules evaluation order. (compute the
-        schedule without indirect demand first)
-        It also made the link between schedules even if some intermediate BoM
-        levels are hidden. (e.g. B1 -1-> B2 -1-> B3, schedule for B1 and B3
-        are linked even if the schedule for B2 does not exist.)
-        Return a list of namedtuple that represent on top the schedules without
-        indirect demand and on lowest leaves the schedules that are the most
-        influenced by the others.
-        """
-        boms = self.env['mrp.bom'].search([
-            '|',
-            ('product_id', 'in', self.mapped('product_id').ids),
-            '&',
-            ('product_id', '=', False),
-            ('product_tmpl_id', 'in', self.mapped('product_id.product_tmpl_id').ids),
-            ('bom_type', '=', 'normal')
-        ])
-        bom_lines_by_product = defaultdict(lambda: self.env['mrp.bom'])
-        bom_lines_by_product_tmpl = defaultdict(lambda: self.env['mrp.bom'])
-        for bom in boms:
-            if bom.product_id:
-                if bom.product_id not in bom_lines_by_product:
-                    bom_lines_by_product[bom.product_id] = bom
-            else:
-                if bom.product_tmpl_id not in bom_lines_by_product_tmpl:
-                    bom_lines_by_product_tmpl[bom.product_tmpl_id] = bom
-
-        Node = namedtuple('Node', ['product', 'ratio', 'children'])
-        indirect_demand_trees = {}
-        product_visited = {}
-
-        def _get_product_tree(product, ratio):
-            product_tree = product_visited.get(product)
-            if product_tree:
-                return Node(product_tree.product, ratio, product_tree.children)
-
-            product_tree = Node(product, ratio, [])
-            product_boms = (bom_lines_by_product[product] | bom_lines_by_product_tmpl[product.product_tmpl_id]).sorted(
-                'sequence')[:1]
-            if not product_boms:
-                product_boms = self.env['mrp.bom']._bom_find(product=product) or self.env['mrp.bom']
-            for line in product_boms.bom_line_ids:
-                line_qty = line.product_uom_id._compute_quantity(line.product_qty, line.product_id.uom_id)
-                bom_qty = line.bom_id.product_uom_id._compute_quantity(line.bom_id.product_qty,
-                                                                       line.bom_id.product_tmpl_id.uom_id)
-                ratio = line_qty / bom_qty
-                tree = _get_product_tree(line.product_id, ratio)
-                product_tree.children.append(tree)
-                if line.product_id in indirect_demand_trees:
-                    del indirect_demand_trees[line.product_id]
-            product_visited[product] = product_tree
-            return product_tree
-
-        for product in self.mapped('product_id'):
-            if product in product_visited:
-                continue
-            indirect_demand_trees[product] = _get_product_tree(product, 1.0)
-
-        return [tree for tree in indirect_demand_trees.values()]
+    # def _get_indirect_demand_tree(self):
+    #     """ Get the tree architecture for all the BoM and BoM line that are
+    #     related to production schedules in self. The purpose of the tree:
+    #     - Easier traversal than with BoM and BoM lines.
+    #     - Allow to determine the schedules evaluation order. (compute the
+    #     schedule without indirect demand first)
+    #     It also made the link between schedules even if some intermediate BoM
+    #     levels are hidden. (e.g. B1 -1-> B2 -1-> B3, schedule for B1 and B3
+    #     are linked even if the schedule for B2 does not exist.)
+    #     Return a list of namedtuple that represent on top the schedules without
+    #     indirect demand and on lowest leaves the schedules that are the most
+    #     influenced by the others.
+    #     """
+    #     boms = self.env['mrp.bom'].search([
+    #         '|',
+    #         ('product_id', 'in', self.mapped('product_id').ids),
+    #         '&',
+    #         ('product_id', '=', False),
+    #         ('product_tmpl_id', 'in', self.mapped('product_id.product_tmpl_id').ids),
+    #         ('bom_type', '=', 'normal')
+    #     ])
+    #     bom_lines_by_product = defaultdict(lambda: self.env['mrp.bom'])
+    #     bom_lines_by_product_tmpl = defaultdict(lambda: self.env['mrp.bom'])
+    #     for bom in boms:
+    #         if bom.product_id:
+    #             if bom.product_id not in bom_lines_by_product:
+    #                 bom_lines_by_product[bom.product_id] = bom
+    #         else:
+    #             if bom.product_tmpl_id not in bom_lines_by_product_tmpl:
+    #                 bom_lines_by_product_tmpl[bom.product_tmpl_id] = bom
+    #
+    #     Node = namedtuple('Node', ['product', 'ratio', 'children'])
+    #     indirect_demand_trees = {}
+    #     product_visited = {}
+    #
+    #     def _get_product_tree(product, ratio):
+    #         product_tree = product_visited.get(product)
+    #         if product_tree:
+    #             return Node(product_tree.product, ratio, product_tree.children)
+    #
+    #         product_tree = Node(product, ratio, [])
+    #         product_boms = (bom_lines_by_product[product] | bom_lines_by_product_tmpl[product.product_tmpl_id]).sorted(
+    #             'sequence')[:1]
+    #         if not product_boms:
+    #             product_boms = self.env['mrp.bom']._bom_find(product=product) or self.env['mrp.bom']
+    #         for line in product_boms.bom_line_ids:
+    #             line_qty = line.product_uom_id._compute_quantity(line.product_qty, line.product_id.uom_id)
+    #             bom_qty = line.bom_id.product_uom_id._compute_quantity(line.bom_id.product_qty,
+    #                                                                    line.bom_id.product_tmpl_id.uom_id)
+    #             ratio = line_qty / bom_qty
+    #             tree = _get_product_tree(line.product_id, ratio)
+    #             product_tree.children.append(tree)
+    #             if line.product_id in indirect_demand_trees:
+    #                 del indirect_demand_trees[line.product_id]
+    #         product_visited[product] = product_tree
+    #         return product_tree
+    #
+    #     for product in self.mapped('product_id'):
+    #         if product in product_visited:
+    #             continue
+    #         indirect_demand_trees[product] = _get_product_tree(product, 1.0)
+    #
+    #     return [tree for tree in indirect_demand_trees.values()]
 
     def _get_replenish_qty(self, after_forecast_qty):
         """ Modify the quantity to replenish depending the min/max and targeted
